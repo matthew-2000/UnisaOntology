9d209b9eba9aa304f4995e0904f017d7d2eeeb92
matthew-2000 <m.ercolino1@studenti.unisa.it>
2024-06-26 15:35:23 +0200

    add test page index.html

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 const FUSEKI_URL = 'http://localhost:3030/unisa/query';
+
+// Serve il file HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // Funzione per eseguire una query SPARQL
 async function executeSparqlQuery(query) {
