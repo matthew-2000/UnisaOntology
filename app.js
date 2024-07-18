35287b40d268d8221d225c6836e2b539cfb0dd29
matthew-2000 <m.ercolino1@studenti.unisa.it>
2024-07-18 16:56:14 +0200

    update web app

--- app.js
+++ app.js
@@ -85,20 +85,23 @@ app.get('/corsi-di-studio', async (req, res) => {
 
 app.get('/insegnamenti', async (req, res) => {
   const corsoDiStudio = req.query.corsoDiStudio; // Passa il nome del corso di studio come query param
+
+  if (!corsoDiStudio) {
+    return res.status(400).json({ error: "Nome del corso di studio non specificato" });
+  }
+
   const query = `
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX : <http://www.semanticweb.org/ontology#>    
 
-    SELECT ?nomeInsegnamento ?cfu ?descrizione
+    SELECT ?nomeInsegnamento
     WHERE {
-          ?corso rdf:type/rdfs:subClassOf* :CorsoDiStudio ;
-            :nome "${corsoDiStudio}";
-            :comprendeInsegnamento ?insegnamento.
-      ?insegnamento :nome ?nomeInsegnamento;
-                     :CFU ?cfu;
-                     :descrizione ?descrizione.
+      ?corso rdf:type/rdfs:subClassOf* :CorsoDiStudio ;
+             :nome "${corsoDiStudio}";
+             :comprendeInsegnamento ?insegnamento.
+      ?insegnamento :nome ?nomeInsegnamento.
     }
   `;
   try {
